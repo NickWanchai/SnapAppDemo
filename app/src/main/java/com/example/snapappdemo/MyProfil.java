bb7c01e22307f6633158ec4bc416769424b43244
nickwanchai <wanchai93@hotmail.dk>
2021-05-21 11:04:54 +0200

    med nye comments2

--- app/src/main/java/com/example/snapappdemo/MyProfil.java
+++ app/src/main/java/com/example/snapappdemo/MyProfil.java
@@ -2,6 +2,7 @@ package com.example.snapappdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,9 @@ import com.example.snapappdemo.repo.Repo;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyProfil extends AppCompatActivity{
 
     //Viser tekst
@@ -20,16 +24,20 @@ public class MyProfil extends AppCompatActivity{
 
     //redigere i tekst
     private EditText usernameEditText, nameEditText, emailEditText, bioEditText;
-    private Button button;
+    private Button saveBtn;
 
 
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
-    private DatabaseReference root = db.getReference();
+    private DatabaseReference root = db.getReference().child("Profil");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profil);
+
+        //root.setValue("hey there, its a test");
 
         usernameEditText = findViewById(R.id.usernameText);
         //usernameEditText = findViewById(R.id.usernameText);
@@ -43,26 +51,26 @@ public class MyProfil extends AppCompatActivity{
         bioEditText = findViewById(R.id.bioText);
         //bioEditText = findViewById(R.id.bioText);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = usernameEditText.getText().toString();
-                root.setValue(username);
-            }
-        });
-
 
     }
 
-//    public void SaveProfilPressed(View view){
-//        //gemmer den text som vi har skrevet ind og erstatter den gamle.
-////        usernameText.setText(usernameEditText.getText());
-////        nameText.setText(nameEditText.getText());
-////        emailText.setText(emailEditText.getText());
-////        bioText.setText(bioEditText.getText());
-////        ProfilRepo.profilRepo().profilInfo(usernameText, nameText, emailText, bioText);
-//        System.out.println("færdig");
-//    }
+    public void SaveProfilPressed(View view){
+        String username = usernameEditText.getText().toString();
+        String name = nameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String biografi = bioEditText.getText().toString();
+
+        Map<String, String> profilMap = new HashMap<>();
+
+        profilMap.put("username", username);
+        profilMap.put("name", name);
+        profilMap.put("email", email);
+        profilMap.put("biografi", biografi);
+
+        root.setValue(profilMap);
+
+        System.out.println("færdig");
+    }
 
 //    @Override
 //    public void update(Object o) {
