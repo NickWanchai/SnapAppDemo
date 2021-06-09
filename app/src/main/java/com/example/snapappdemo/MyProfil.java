f063bb3c25406b4466e75eb0c7d8ab68925ae776
nickwanchai <wanchai93@hotmail.dk>
2021-06-09 09:16:36 +0200

    med nye comments4

--- app/src/main/java/com/example/snapappdemo/MyProfil.java
+++ app/src/main/java/com/example/snapappdemo/MyProfil.java
@@ -16,7 +16,6 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class MyProfil extends AppCompatActivity {
 
     //Edit text for at redigere i tekst
